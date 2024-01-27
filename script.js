7a7ecbf7578175664429c5015bfbc13c667dd6e9
Kinga Kohari <kinguca@yahoo.com>
2024-01-27 21:50:20 +0000

    Header + footer handled using JS

---
+++ script.js
@@ -0,0 +1,33 @@
+function loadEvent() {
+    
+    // Insert the navigation bar on every HTML page
+    document.body.insertAdjacentHTML("afterbegin", `
+        <header>
+            <nav id="navbar">
+                <h4>Acme Inc.</h4>
+                <ul>
+                    <li><a href="szolgaltatasok.html">Szolgáltatásaink</a></li>
+                    <li><a href="ceginformacio.html">Céginformáció</a></li>
+                    <li><a href="kapcsolat.html">Kapcsolat</a></li>
+                </ul>
+            </nav>
+        </header>
+    `);
+
+    // Add the 'active' class to the current page link in the navigation
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+
+    // Insert the footer on every HTML page
+    document.body.insertAdjacentHTML("beforeend", `
+        <footer id="footer">
+            <h2>Acme</h2>
+        </footer>
+    `);
+}
+
+window.addEventListener("load", loadEvent);
