0a017046ae4f5b2c810e66e75f6f89d6e23911d8
Kinga Kohari <kinguca@yahoo.com>
2024-01-29 18:26:13 +0000

    HTML details for about section with basic CSS

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function loadEvent() {
             <nav id="navbar">
                 <h1>Acme Inc.</h1>
                 <ul>
-                    <li><a href="./szolgaltatasok.html">Szolgáltatásaink</a></li>
                     <li><a href="./ceginformacio.html">Céginformáció</a></li>
+                    <li><a href="./szolgaltatasok.html">Szolgáltatásaink</a></li>
                     <li><a href="./kapcsolat.html">Kapcsolat</a></li>
                 </ul>
             </nav>
@@ -15,16 +15,16 @@ function loadEvent() {
     `);
 
     // Add the 'active' class to the current page link in the navigation
-    const navLinks = document.querySelectorAll('nav ul li a');
+    const navLinks = document.querySelectorAll("nav ul li a");
     navLinks.forEach(link => {
         if (link.href === window.location.href) {
-            link.classList.add('active');
+            link.classList.add("active");
         }
     });
 
     // Insert the footer on every HTML page
     document.body.insertAdjacentHTML("beforeend", `
-        <footer id="footer">
+        <footer id="footer-btm">
             <h2>Acme</h2>
         </footer>
     `);
