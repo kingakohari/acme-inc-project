701a3cb2c8f22633fb94aebe03713bc90428e0c2
Kinga Kohari <kinguca@yahoo.com>
2024-02-02 14:59:06 +0000

    Gradient applied consistently

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function loadEvent() {
-    
-    // Insert the navigation bar on every HTML page
-    document.body.insertAdjacentHTML("afterbegin", `
+  const gradientContainer = document.querySelector(".gradient-container");
+
+  // Insert the navigation bar on every HTML page
+  if (gradientContainer) {
+    gradientContainer.insertAdjacentHTML(
+      "afterbegin",
+      `
         <header>
             <nav class="navbar">
                 <h1><a href="./index.html"><span>Acme</span> Inc.</a></h1>
@@ -12,22 +16,29 @@ function loadEvent() {
                 </ul>
             </nav>
         </header>
-    `);
+    `
+    );
 
     // Add the 'active' class to the current page link in the navigation
     const navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach(link => {
-        if (link.href === window.location.href) {
-            link.classList.add("active");
-        }
+    navLinks.forEach((link) => {
+      if (link.href === window.location.href) {
+        link.classList.add("active");
+      }
     });
 
     // Insert the footer on every HTML page
-    document.body.insertAdjacentHTML("beforeend", `
+    gradientContainer.insertAdjacentHTML(
+      "beforeend",
+      `
         <footer id="footer-btm">
             <h2>Acme</h2>
         </footer>
-    `);
+    `
+    );
+  } else {
+    console.error("Gradient container not found");
+  }
 }
 
 window.addEventListener("load", loadEvent);
