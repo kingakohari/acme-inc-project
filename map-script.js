3da71a8374777dab5fc025bd6ab9ac4dda818c34
Kinga Kohari <kinguca@yahoo.com>
2024-02-08 15:45:44 +0000

    Refactoring

--- map-script.js
+++ map-script.js
@@ -3,19 +3,19 @@
 const mapElement = document.getElementById("map");
 
 async function initMap() {
-  /*     const { Map } = await google.maps.importLibrary("maps");
+  const { Map } = await google.maps.importLibrary("maps");
     
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker"); */
+  const { Marker } = await google.maps.importLibrary("marker");
 
   const location = { lat: 66.54028947695342, lng: 25.843774799713106 };
 
-  const map = new google.maps.Map(mapElement, {
+  const map = new Map(mapElement, {
     center: location,
     zoom: 15,
     mapId: "DEMO_MAP_ID",
   });
 
-  const marker = new google.maps.Marker({
+  const marker = new Marker({
     map,
     position: location,
     title: "ACME Inc.",
