ee32c2ab3fa671a0262c64071de0082d98f165ba
Kinga Kohari <kinguca@yahoo.com>
2024-02-08 13:54:29 +0000

    map-script.js OK

--- map-script.js
+++ map-script.js
@@ -3,26 +3,25 @@
 const mapElement = document.getElementById("map");
 
 async function initMap() {
-    /*     const { Map } = await google.maps.importLibrary("maps");
+  /*     const { Map } = await google.maps.importLibrary("maps");
     
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker"); */ 
-    
-        const location = { lat: 66.54028947695342, lng: 25.843774799713106 }; 
-    
-        const map = new google.maps.Map(mapElement, {
-          center: location, 
-          zoom: 15,
-          mapId: 'DEMO_MAP_ID'
-        });
-    
-        const marker = new google.maps.Marker({
-          map,
-          position: location,
-          title: "ACME Inc.",
-        });
-      }
-  
-      if(mapElement) {
-      initMap();
-      }
-  
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker"); */
+
+  const location = { lat: 66.54028947695342, lng: 25.843774799713106 };
+
+  const map = new google.maps.Map(mapElement, {
+    center: location,
+    zoom: 15,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new google.maps.Marker({
+    map,
+    position: location,
+    title: "ACME Inc.",
+  });
+}
+
+if (mapElement) {
+  initMap();
+}
